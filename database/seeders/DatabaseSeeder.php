b8503a0ce5dfacbe57acb362ca4a49129cf87d73
Marcus Vinicius(Vinny) <viniciuskawakami@hotmail.com>
2023-01-27 06:15:25 -0300

    add migrations and seedersand factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(0)->create();
-
+         \App\Models\User::factory(20)->create();
+        \App\Models\Product::factory(100)->create();
          \App\Models\User::factory()->create([
              'name' => 'Test User',
              'email' => 'test@example.com',
